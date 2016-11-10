8dd556dea1c2b015a627f3f6f937fd144c705abb
钟宇强 <zhongyq@51weker.cn>
2016-11-10 10:18:39 +0800

    监听路由变化bug修复

--- ng/main.js
+++ ng/main.js
@@ -6,24 +6,37 @@
 angular.module('mainMdl', [])
   .controller('mainCtl', mainCtl);
 
-mainCtl.$inject = ['$scope', '$state'];
-function mainCtl($scope, $state) {
+mainCtl.$inject = ['$scope', '$location', '$state'];
+function mainCtl($scope, $location, $state) {
   var mainVm = this;
   mainVm.asideArr = [
     {title: '客户管理', subTitle:'Customer Management', icon: 'user', sref: 'customer.manage', path: '/customer/manage', isActive: true},
-    {title: '订单管理', subTitle:'Orders Management', icon: '', sref: 'order.receipt', path: '/customer/receipt', isActive: false},
-    {title: '售后服务', subTitle:'After-sales Service', icon: '', sref: 'service.setup', path: '/customer/setup', isActive: false},
-    {title: '配置管理', subTitle:'Configure Management', icon: '', sref: 'config.banner', path: '/customer/banner', isActive: false},
+    {title: '订单管理', subTitle:'Orders Management', icon: '', sref: 'order.receipt', path: '/order/receipt', isActive: false},
+    {title: '售后服务', subTitle:'After-sales Service', icon: '', sref: 'service.setup', path: '/service/setup', isActive: false},
+    {title: '配置管理', subTitle:'Configure Management', icon: '', sref: 'config.banner', path: '/config/banner', isActive: false},
     //{title: '用户反馈', subTitle:'User Feedback', icon: '', sref: '', isActive: false},
     //{title: '图片上传', subTitle:'Picture Upload', icon: '', sref: '', isActive: false},
     {title: '登陆日志', subTitle:'Login log', icon: '', sref: 'log.record', path: '/customer/record', isActive: false},
-    {title: '账号管理', subTitle:'Account Management', icon: '', sref: 'account.update', path: '/customer/update', isActive: false},
+    {title: '账号管理', subTitle:'Account Management', icon: '', sref: 'account.update', path: '/account/update', isActive: false},
   ];
   mainVm.currentNav = {title: '客户管理', subTitle:'Customer Management', icon: 'user', sref: '', path: '/customer/manage', isActive: true};
   mainVm.asideCollapse = false;
-
   mainVm.switchNavItem = switchNavItem;
+  // $locationChangeSuccess: 监听路由变化事件
+  $scope.$on("$locationChangeSuccess", function () {
+    checkUrl();
+  })
 
+  function checkUrl(){
+    var path = $location.path();
+    var arr = mainVm.asideArr;
+    for(var i=0;i<arr.length;i++){
+      mainVm.asideArr[i].isActive = false;
+      if(path === arr[i].path){
+        mainVm.asideArr[i].isActive = true
+      }
+    }
+  }
   function switchNavItem(index){
     mainVm.currentNav = mainVm.asideArr[index];
     $state.go(mainVm.currentNav.sref)
